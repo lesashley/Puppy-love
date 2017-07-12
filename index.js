f900891e43cf1a04f688e166d62b158f7be3fe84
lesahley <lesashley56@gmail.com>
2017-07-12 12:19:53 -0500

    Package.json

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
+app.use('/', express.static('public'));
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/'));
-app.set('', __dirname + '/');
-app.set('view engine', 'ejs');
-app.get('/', function(request, response) {
-  response.render('index');
-});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
